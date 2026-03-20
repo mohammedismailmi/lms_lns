f68d053b4a714e61196f7f5e392708a9986e69ac
mohammedismailmi <mohammedismail0602@gmail.com>
2026-03-20 09:54:31 +0530

    if few chnges are done

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: { host: '127.0.0.1', port: 5173 },
   plugins: [
     react(),
     tailwindcss(),
